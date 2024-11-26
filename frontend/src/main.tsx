42c69f122e3b51a9634a0013f40963a3d077c780
Aleksandr <33900974+vislofie@users.noreply.github.com>
2024-11-26 17:04:13 +0300

    customized scrolls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './main.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
