3407ffa3faef8688fcf0ca38b1ec2a236f67a7f7
Александр Карпов <karpovai@trueconf.ru>
2025-04-22 11:46:42 +0300

    DOMAIN

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823bf860a04bb0d1511254284985ed982acfb2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785c63937d2959dcdfaf583e01ff3f99e60c90ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
